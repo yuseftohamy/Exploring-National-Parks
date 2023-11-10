c797ed5a702ed79dc431e54110e29c07e7c42db4
DanielEap <danieliseap@gmail.com>
2023-11-10 12:24:40 -0500

    Refactoring code for cleanliness/efficiency
    Cleaned up html file to represent standard react setups.
    Created style folder under src. moved the styling from old style.css and api.css into new activitiesList.css to match css conventions.
    Refactored NPSAPI.js into api call and jsx component;
    visual component now under /ParkSearch/Components/ActivitiesList.jsx
    API call now in /ParkSearch/Functionality/Activities.js
    Fixed index.js to match conventions

---
+++ exploring-national-parks/src/ParkSearch.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ActivitiesList from './ParkSearch/Components/ActivitiesList.jsx';
+function ParkSearch(){
+    return(
+    <div>
+        <ActivitiesList />
+    </div>
+    );
+}
+
+export default ParkSearch;
