1923a04967ce27d3259932a8f808dbe2606eaf3f
Sarina Curtis <tuk56008@temple.edu>
2023-11-15 01:39:45 -0500

    Add sample welcome text

--- exploring-national-parks/src/HomePage/Components/Welcome.jsx
+++ exploring-national-parks/src/HomePage/Components/Welcome.jsx
@@ -3,7 +3,14 @@ import React from 'react'
 const Welcome = () => {
     return (
         <div>
-            <h5 className = "welcome-title">National Parks Explorer</h5>
+            <h1 className = "welcome-title">National Parks Explorer</h1>
+            <p className = "welcome-text">
+                Sample Welcome Text: Welcome to the National Parks Explorer! This app is designed to help you find the perfect national park for your next trip. 
+                Click on the "Park Search" button to search for a park by activity, or click on the "Plan A Trip" button to plan a trip to a park you've already selected.
+            </p>
+            <div>
+
+            </div>
         </div>
     )
 }
