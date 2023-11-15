9dfd366b104fcc7ee0e5e03f3b51b6f90b2f643c
Sarina Curtis <tuk56008@temple.edu>
2023-11-14 22:41:09 -0500

    Add Welcome Page Title and Buttons

--- exploring-national-parks/src/HomePage/Components/Welcome.jsx
+++ exploring-national-parks/src/HomePage/Components/Welcome.jsx
@@ -4,7 +4,11 @@ import yosemite from '../Assets/yosemite.jpg';
 
 const Welcome = () => {
     return (
-        <div style={{backgroundImage: `url(${yosemite})`, backgroundSize: 'cover', backgroundPosition: 'center', height: '100vh', fontSize:'10rem'}}>Welcome</div>
+        <div style={{backgroundImage: `url(${yosemite})`, backgroundSize: 'cover', backgroundPosition: 'center', height: '100vh', fontSize:'10rem'}}>
+            <h5>National Parks Explorer</h5>
+            <button>Park Search</button>
+            <button>Plan A Trip</button>
+            </div>
     )
 }
 
