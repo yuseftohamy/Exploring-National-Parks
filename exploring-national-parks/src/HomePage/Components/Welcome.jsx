39516a0d955eb51d95c991cc78ca857e09ef0d06
Sarina Curtis <tuk56008@temple.edu>
2023-11-15 00:31:37 -0500

    Update buttons in homepage

--- exploring-national-parks/src/HomePage/Components/Welcome.jsx
+++ exploring-national-parks/src/HomePage/Components/Welcome.jsx
@@ -6,10 +6,16 @@ const Welcome = () => {
     return (
         <div style={{backgroundImage: `url(${yosemite})`, backgroundSize: 'cover', backgroundPosition: 'center', height: '100vh', fontSize:'10rem'}}>
             <h5 className = "welcome-title">National Parks Explorer</h5>
-            <button>Park Search</button>
-            <button>Plan A Trip</button>
+            <div className = "homepage-button-wrapper">
+                <button className = "homepage-button" onClick = {() => handleClick("search")}>Park Search</button>
+                <button className = "homepage-button" onClick = {() => handleClick("plan")}>Plan A Trip</button>
             </div>
+        </div>
     )
+}
+
+function handleClick(buttonType) {
+    buttonType === "search" ? window.location.href = "/parksearch" : console.log("plan");
 }
 
 export default Welcome
