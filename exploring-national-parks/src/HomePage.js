d8bde7129aef2036a19a54fefc91d3e096752a34
Sarina Curtis <tuk56008@temple.edu>
2023-11-14 23:04:32 -0500

    Fix HomePage Title's Margin

--- exploring-national-parks/src/HomePage.js
+++ exploring-national-parks/src/HomePage.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import Welcome from './HomePage/Components/Welcome'
+import './Style/homepage.css'
 const HomePage = () => {
   return (
     // <Navbar/>
-    <div>
-        <h1>Test Hello</h1>
+    <div className = "home-page">
+        {/* <h1>Test Hello</h1> */}
         <Welcome/>
     </div>
   )
--- exploring-national-parks/src/HomePage/Components/Welcome.jsx
+++ exploring-national-parks/src/HomePage/Components/Welcome.jsx
@@ -5,7 +5,7 @@ import yosemite from '../Assets/yosemite.jpg';
 const Welcome = () => {
     return (
         <div style={{backgroundImage: `url(${yosemite})`, backgroundSize: 'cover', backgroundPosition: 'center', height: '100vh', fontSize:'10rem'}}>
-            <h5>National Parks Explorer</h5>
+            <h5 className = "welcome-title">National Parks Explorer</h5>
             <button>Park Search</button>
             <button>Plan A Trip</button>
             </div>
