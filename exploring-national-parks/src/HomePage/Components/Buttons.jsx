f0abba34a692f398d5ea59f7300642ade51da139
DanielEap <danieliseap@gmail.com>
2023-11-16 13:27:56 -0500

    added routing to the homepage buttons

--- exploring-national-parks/src/HomePage/Components/Buttons.jsx
+++ exploring-national-parks/src/HomePage/Components/Buttons.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom'
 const Buttons = () => {
     return (
         <div className = "homepage-button-wrapper">
-                <button className = "homepage-button" onClick = {() => handleClick("search")}>Park Search</button>
-                <button className = "homepage-button" onClick = {() => handleClick("plan")}>Plan A Trip</button>
+                {/* <button className = "homepage-button" onClick = {() => handleClick("ParkSearch")}>Park Search</button>
+                <button className = "homepage-button" onClick = {() => handleClick("ParkPlan")}>Plan A Trip</button> */}
+                <button className="homepage-button">
+                    <Link  to='/ParkSearch'>Park Search</Link>
+                </button>
+                <button className="homepage-button">
+                    <Link className="homepage-button" to='/ParkPlan'>Plan A Trip</Link>
+                </button>
         </div>
     )
 }
