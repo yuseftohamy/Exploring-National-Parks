9c07bbade8161dc648fd6751030ce0100b56373d
DanielEap <danieliseap@gmail.com>
2023-11-30 15:21:33 -0500

    catch for schedule

--- exploring-national-parks/src/ParkPlan/Components/Schedule.jsx
+++ exploring-national-parks/src/ParkPlan/Components/Schedule.jsx
@@ -7,27 +7,32 @@ const Schedule = ({ dates }) => {
 
     useEffect(() => {
         if (dates !== null) {
-            setStartDate(dates[0].toLocaleDateString());
-            if (dates[1]) {
-                setEndDate(dates[1].toLocaleDateString());
-            } else {
-                setEndDate(startDate);
+            try {
+
+
+                setStartDate(dates[0].toLocaleDateString());
+                if (dates[1]) {
+                    setEndDate(dates[1].toLocaleDateString());
+                } else {
+                    setEndDate(startDate);
+                }
+
+                // Convert startDate and endDate to Date objects
+                const start = new Date(startDate);
+                const end = new Date(endDate);
+
+                // Create an array of dates between start and end
+                const dateArray = [];
+                while (start <= end) {
+                    dateArray.push(new Date(start));
+                    start.setDate(start.getDate() + 1);
+                }
+
+                // Now dateArray contains all the dates between startDate and endDate
+                //console.log(dateArray);
+                setDatesArray(dateArray);
+            } catch (error) {
             }
-
-            // Convert startDate and endDate to Date objects
-            const start = new Date(startDate);
-            const end = new Date(endDate);
-
-            // Create an array of dates between start and end
-            const dateArray = [];
-            while (start <= end) {
-                dateArray.push(new Date(start));
-                start.setDate(start.getDate() + 1);
-            }
-
-            // Now dateArray contains all the dates between startDate and endDate
-            //console.log(dateArray);
-            setDatesArray(dateArray);
         }
     }, [dates, startDate, endDate]);
 
@@ -43,13 +48,13 @@ const Schedule = ({ dates }) => {
             <div className='dates-container'>
                 {datesArray.map((date, index) => (
                     <div key={index} className='individual-date-container'>
-                         <h3>{date.toLocaleDateString()}</h3>
-                         <h3>Morning</h3>
-                            <h3>Afternoon</h3>
-                            <h3>Evening</h3>
-                            <br/>   
+                        <h3>{date.toLocaleDateString()}</h3>
+                        <h3>Morning</h3>
+                        <h3>Afternoon</h3>
+                        <h3>Evening</h3>
+                        <br />
                     </div>
-                   
+
                 ))}
             </div>
         </div>
