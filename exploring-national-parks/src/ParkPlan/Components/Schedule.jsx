6ef790e865c7102566b7240732780d4f2916e940
eross04 <erinross@temple.edu>
2023-12-05 17:08:57 -0500

    Currently working barebones park planning

--- exploring-national-parks/src/ParkPlan/Components/Schedule.jsx
+++ exploring-national-parks/src/ParkPlan/Components/Schedule.jsx
@@ -47,7 +47,7 @@ const Schedule = ({ dates, parkCode, activities}) => {
                 console.log("dates", dates);
                 const activitiesToDo = await FetchThingsToDo(parkCode, activities, dates);
                 setThingsToDo(activitiesToDo);
-                console.log(thingsToDo);
+                console.log("things to do", thingsToDo);
             } catch (error) {
                 // Handle the error, if needed
                 console.log(error);
@@ -63,22 +63,68 @@ const Schedule = ({ dates, parkCode, activities}) => {
         return null;
     }
 
+    const ReturnSchedule = () => {
+        var thingsIndex = 0;
+        console.log("first thing index",thingsIndex);
+        const morningActivities = [];
+        const dayActivities = [];
+        const eveningActivites = [];
+        for(let i =0; i<datesArray.length; i++) {
+            //Fill in morning array
+            console.log("in foreach",datesArray[i]);
+            morningActivities.push(thingsToDo && thingsToDo[thingsIndex]);
+            console.log("in foreach 2",thingsToDo && thingsToDo[thingsIndex]);
+            thingsIndex++;
+            if(thingsIndex === thingsToDo?.length) {
+                thingsIndex=0;
+            }
+            console.log("things index",thingsIndex);
+
+            //Fill in day array
+            console.log("in foreach",datesArray[i]);
+            dayActivities.push(thingsToDo && thingsToDo[thingsIndex]);
+            console.log("in foreach 2",thingsToDo && thingsToDo[thingsIndex]);
+            thingsIndex++;
+            if(thingsIndex === thingsToDo?.length) {
+                thingsIndex=0;
+            }
+            console.log("things index",thingsIndex);
+
+            //Fill in evening array
+            console.log("in foreach",datesArray[i]);
+            eveningActivites.push(thingsToDo && thingsToDo[thingsIndex]);
+            console.log("in foreach 2",thingsToDo && thingsToDo[thingsIndex]);
+            thingsIndex++;
+            if(thingsIndex === thingsToDo?.length) {
+                thingsIndex=0;
+            }
+            console.log("things index",thingsIndex);
+        };
+        return (datesArray.map((date, index) => (
+            <div key={index} className='individual-date-container'>
+                <h3>{date.toLocaleDateString()}</h3>
+                <h3>Morning</h3>
+                {morningActivities && morningActivities[index]?.title}
+                {console.log(morningActivities)}
+                <h3>Afternoon</h3>
+                {dayActivities && dayActivities[index]?.title}
+                {console.log(dayActivities)}
+                <h3>Evening</h3>
+                {eveningActivites && eveningActivites[index]?.title}
+                {console.log(eveningActivites)}
+                <br />
+            </div>
+
+        )));
+    }
+
     return (
         <div>
             <div>
                 {datesArray && datesArray.length > 0 ? <h1>Schedule</h1> : null}
             </div>
             <div className='dates-container'>
-                {datesArray.map((date, index) => (
-                    <div key={index} className='individual-date-container'>
-                        <h3>{date.toLocaleDateString()}</h3>
-                        <h3>Morning</h3>
-                        <h3>Afternoon</h3>
-                        <h3>Evening</h3>
-                        <br />
-                    </div>
-
-                ))}
+                <ReturnSchedule />
             </div>
         </div>
     )
