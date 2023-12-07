3a8220c0b8b4043d705d943409f5e06165ebf804
Sarina Curtis <tuk56008@temple.edu>
2023-12-07 18:21:00 -0500

    merge

--- exploring-national-parks/src/ParkInfo.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ParkInfoComponent from './ParkInfo/Components/ParkInfoComponent.jsx';
-function ParkInfo(){
-    return(
-        <ParkInfoComponent />
-    );
-}
-
-export default ParkInfo;
