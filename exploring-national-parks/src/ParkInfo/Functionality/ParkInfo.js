02181ec897b62dd4a899f8a8082970ebca3b553d
Jason Hankins <tul53850@temple.edu>
2023-12-07 15:56:20 -0500

    ParkInfo temporarily remove caching

--- exploring-national-parks/src/ParkInfo/Functionality/ParkInfo.js
+++ exploring-national-parks/src/ParkInfo/Functionality/ParkInfo.js
@@ -1,5 +1,24 @@
 // ParkInfo.js, fetch the list of parks and return info
+
 export const ParkInfo = async (parkCode, page) => {
+  try {
+    await parkCode;
+    await page;
+    var url = 'https://developer.nps.gov/api/v1/parks?api_key=0ilOFP8jTC2LMrwXFTullFqvHyVhBh9aHVW3OWEb&parkCode=' + parkCode + '&start=' + page;
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    const json = await response.json();
+    return json;
+  } catch (error) {
+    console.error(error.message);
+    throw error;
+  }
+};
+
+
+/*export const ParkInfo = async (parkCode, page) => {
   try {
     const cachedData = localStorage.getItem(parkCode);
     if (cachedData) {
@@ -23,3 +42,4 @@ export const ParkInfo = async (parkCode, page) => {
     throw error;
   }
 };
+*/
