e26241ca8df52b352d3bd159e4fda3ef4165d002
DanielEap <danieliseap@gmail.com>
2023-12-07 15:26:27 -0500

    caching

--- exploring-national-parks/src/ParkInfo/Functionality/ParkInfo.js
+++ exploring-national-parks/src/ParkInfo/Functionality/ParkInfo.js
@@ -1,19 +1,25 @@
 // ParkInfo.js, fetch the list of parks and return info
 export const ParkInfo = async (parkCode) => {
-    try {
-      await parkCode;
-      var url = 'https://developer.nps.gov/api/v1/parks?api_key=0ilOFP8jTC2LMrwXFTullFqvHyVhBh9aHVW3OWEb&parkCode=' + parkCode;
-      console.log("url park info",url);
-      const response = await fetch(url);
-      if (!response.ok) {
-        throw new Error('Network response was not ok');
-      }
-      const json = await response.json();
-      return json;
-    } catch (error) {
-      console.error(error.message);
-      throw error;
+  try {
+    const cachedData = localStorage.getItem(parkCode);
+    if (cachedData) {
+      return JSON.parse(cachedData);
     }
-  };
 
-  
+    var url = 'https://developer.nps.gov/api/v1/parks?api_key=0ilOFP8jTC2LMrwXFTullFqvHyVhBh9aHVW3OWEb&parkCode=' + parkCode;
+    console.log("url park info",url);
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    const json = await response.json();
+
+    // Cache the response to localStorage
+    localStorage.setItem(parkCode, JSON.stringify(json));
+
+    return json;
+  } catch (error) {
+    console.error(error.message);
+    throw error;
+  }
+};
--- exploring-national-parks/src/ParkSearch/Functionality/Activities.js
+++ exploring-national-parks/src/ParkSearch/Functionality/Activities.js
@@ -1,6 +1,13 @@
 // Activities.js
 export const Activities = async () => {
     try {
+      const cacheKey = 'activitiesData';
+      let activitiesData = localStorage.getItem(cacheKey);
+  
+      if (activitiesData) {
+        return JSON.parse(activitiesData);
+      }
+  
       const response = await fetch(
         'https://developer.nps.gov/api/v1/activities?api_key=0ilOFP8jTC2LMrwXFTullFqvHyVhBh9aHVW3OWEb'
       );
@@ -8,6 +15,8 @@ export const Activities = async () => {
         throw new Error('Network response was not ok');
       }
       const json = await response.json(); 
+      localStorage.setItem(cacheKey, JSON.stringify(json));
+
       return json;
     } catch (error) {
       console.error(error.message);
