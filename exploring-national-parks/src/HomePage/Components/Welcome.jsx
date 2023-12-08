0b6383424f533e008527f30b9e635e3dce6c158f
Sarina Curtis <tuk56008@temple.edu>
2023-12-07 19:44:25 -0500

    Update Homepage text

--- exploring-national-parks/src/HomePage/Components/Welcome.jsx
+++ exploring-national-parks/src/HomePage/Components/Welcome.jsx
@@ -4,9 +4,9 @@ import React from 'react'
 const Welcome = () => {
     return (
         <div className = "welcome">
-            <h1 className = "welcome-title">National Parks Explorer</h1>
+            <h1 className = "welcome-title">Explore National Parks</h1>
             <p className = "welcome-text">
-                Welcome to the National Parks Explorer! This app is designed to help you find the perfect national park for your next trip. 
+                Welcome to Exploring National Parks! This web app is designed to help you find the perfect national park for your next trip. 
                 Click on the "Park Search" button to search for a park by activity, or click on the "Plan A Trip" button to plan a trip to a park you've already selected.
             </p>
             <div>
