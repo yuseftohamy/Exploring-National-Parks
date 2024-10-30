41af1cbd2d4476248b01c8b3ef53c403f0335a06
NeverEverFelix <felixmftc@icloud.com>
2024-10-30 13:04:37 -0400

    changes to index.js

--- exploring-national-parks/src/index.js
+++ exploring-national-parks/src/index.js
@@ -19,6 +19,7 @@ import ParkSearch from './ParkSearch.js';
 import ParkInfo from './ParkInfo.js';
 import HomePage from './HomePage.js';
 import ParkPlan from './ParkPlan.js';
+import HelpPage from './helpPage.js';
 import Navbar from './GlobalComponents/Navbar.jsx';
 import {BrowserRouter as Router, Routes,Route} from "react-router-dom";
 import Footer from './GlobalComponents/Footer.jsx';
@@ -34,6 +35,7 @@ root.render(
                 <Route path="/ParkSearch" element={<ParkSearch />} />
                 <Route path="/ParkInfo" element={<ParkInfo />} />
                 <Route path="/ParkPlan" element={<ParkPlan />} />
+                <Route path="/help" element={<HelpPage />} /> {/* Add the Help route */}
             </Routes>
             <Footer></Footer>
         </Router>
